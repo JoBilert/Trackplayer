d0611626bb2b0248cf5dcbeb81d2d2a26cd9cea3
JoBilert <bilert@gmail.com>
2022-03-20 11:04:30 +0100

    Volumessliders

--- trackscape_kivy.py
+++ trackscape_kivy.py
@@ -1,9 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
 from kivy.properties import ObjectProperty
 import pygame
